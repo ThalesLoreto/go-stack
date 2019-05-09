4bb0c599258b71ad1930c79aea8d6a2d3d4d3d64
ThalesLoreto <thalesmulti3@gmail.com>
2019-05-09 00:03:35 -0300

    desafio inicial go node

---
+++ go-node/desafio1/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+const middleWare = (req, res, next) => {
+  const { age } = req.query
+
+  if (!age) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+
+app.get('/', (req, res) => {
+  return res.render('home')
+})
+
+app.get('/major', middleWare, (req, res) => {
+  const { age } = req.query
+  return res.render('major', { age })
+})
+
+app.get('/minor', middleWare, (req, res) => {
+  const { age } = req.query
+  return res.render('minor', { age })
+})
+
+app.post('/check', (req, res) => {
+  const { age } = req.body
+
+  if (age >= 18) {
+    return res.redirect(`/major?age=${age}`)
+  } else {
+    return res.redirect(`/minor?age=${age}`)
+  }
+})
+
+app.listen(3000)
