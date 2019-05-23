d5e57e9414854a0bbd49578d64eba3ace3497688
ThalesLoreto <thalesmulti3@gmail.com>
2019-05-23 14:24:24 -0300

    react concepts - state / setstate

--- go-react/concepts/src/index.js
+++ go-react/concepts/src/index.js
@@ -3,40 +3,35 @@ import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
 
 class Button extends Component {
+  static defaultProps = {
+    children: "Salvar"
+  };
+
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.string
+  };
+
   render() {
-    return (
-      <a href="" onClick={this.props.onClick}>
-        {this.props.children}
-      </a>
-    );
+    return <button onClick={this.props.onClick}>{this.props.children}</button>;
   }
 }
 
-Button.defaultProps = {
-  children: "Salvar"
-};
-
-Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  children: PropTypes.string
-};
-
 class App extends Component {
-  handleClick() {
-    alert("Botão enviar clicado!");
-  }
+  state = {
+    counter: 0
+  };
+
+  handleClick = () => {
+    this.setState({ counter: (this.state.counter += 1) });
+  };
 
   render() {
     return (
       <Fragment>
         <h1>Hello World</h1>
-        <Button
-          onClick={() => {
-            alert("Botão Salvar");
-          }}
-        />
-        <br />
-        <Button onClick={this.handleClick}>Enviar</Button>
+        <h1>{this.state.counter}</h1>
+        <Button onClick={this.handleClick}>Somar</Button>
       </Fragment>
     );
   }
