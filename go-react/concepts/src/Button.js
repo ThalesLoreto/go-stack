0c374ad00c14f57067355d523f614033ea4bf4bf
ThalesLoreto <thalesmulti3@gmail.com>
2019-05-23 18:06:34 -0300

    react concepts - end/reformating files

--- go-react/concepts/src/Button.js
+++ go-react/concepts/src/Button.js
@@ -1,13 +1,13 @@
-import React from "react";
-import PropTypes from "prop-types";
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/button-has-type */
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Button = props => (
-  <button onClick={props.onClick}>{props.children}</button>
-);
+const Button = props => <button onClick={props.onClick}>{props.children}</button>;
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
-  children: PropTypes.string
 };
 
 export default Button;
--- go-react/concepts/src/index.js
+++ go-react/concepts/src/index.js
@@ -1,28 +1,29 @@
-import React, { Component, Fragment } from "react";
-import ReactDOM from "react-dom";
+import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
 
-import Button from "./Button";
+import Button from './Button';
 
-import "./style.scss";
+import './style.scss';
 
 class App extends Component {
   state = {
-    counter: 0
+    counter: 0,
   };
 
   handleClick = () => {
+    screen;
     this.setState({ counter: (this.state.counter += 1) });
   };
 
   render() {
     return (
       <Fragment>
-        <h1>Hello World</h1>
-        <h1 style={{ color: "#f00" }}>{this.state.counter}</h1>
+        <h1 className="title">Hello World</h1>
+        <h1 style={{ color: '#f00' }}>{this.state.counter}</h1>
         <Button onClick={this.handleClick}>Somar</Button>
       </Fragment>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
