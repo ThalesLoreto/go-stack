a9881ebe861b843c7beb318f411318aa986518d1
ThalesLoreto <thalesmulti3@gmail.com>
2019-05-23 12:31:50 -0300

    react concepts - set default props and proptypes

--- go-react/concepts/src/index.js
+++ go-react/concepts/src/index.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
 
 class Button extends Component {
@@ -11,15 +12,30 @@ class Button extends Component {
   }
 }
 
+Button.defaultProps = {
+  children: "Salvar"
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.string
+};
+
 class App extends Component {
   handleClick() {
-    alert("Botão clicado!");
+    alert("Botão enviar clicado!");
   }
 
   render() {
     return (
       <Fragment>
         <h1>Hello World</h1>
+        <Button
+          onClick={() => {
+            alert("Botão Salvar");
+          }}
+        />
+        <br />
         <Button onClick={this.handleClick}>Enviar</Button>
       </Fragment>
     );
