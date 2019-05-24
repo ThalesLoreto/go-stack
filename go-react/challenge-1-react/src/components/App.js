c7ae48c5d786983fe68f1d65425424bfe11113a4
ThalesLoreto <thalesmulti3@gmail.com>
2019-05-23 22:27:20 -0300

    react challenge1 - first commit

---
+++ go-react/challenge-1-react/src/components/App.js
@@ -0,0 +1,6 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+export default function App() {
+  return <h1>Hello world</h1>;
+}
---
+++ go-react/challenge-1-react/src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Hello from './components/App';
+
+import './style.scss';
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
